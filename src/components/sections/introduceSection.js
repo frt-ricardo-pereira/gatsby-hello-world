3b3d9fedc4f2cd4c85edcf4e7126fa942a3824af
Ricardo Pereira <ricardo.pereira@weareforeteller.com>
2021-05-20 01:49:51 +0100

    2eq2e2

--- src/components/sections/introduceSection.js
+++ src/components/sections/introduceSection.js
@@ -2,7 +2,8 @@ import React from "react"
 import tw from "twin.macro"
 const TwoColumn = tw.div`flex flex-col lg:flex-row lg:items-center max-w-screen-xl mx-auto py-20 md:py-24 `
 const LeftColumn = tw.div`relative lg:w-5/12 text-center max-w-lg mx-auto lg:max-w-none lg:text-left text-4xl tracking-tight  sm:leading-none  lg:text-4xl`
-const RightColumn = tw.div`relative mt-12 lg:mt-0 flex-1 flex flex-col justify-center lg:self-end`;
+const RightColumn = tw.div`relative mt-12 lg:mt-0 flex-1 flex flex-col justify-center lg:self-end`
+
 export default function IntroduceSection({
   introTitle1,
   introTitle2,
@@ -17,14 +18,12 @@ export default function IntroduceSection({
             {introTitle1 + " "}
             <span class="text-sunset-500">{introTitle2} </span>
           </span>
-       
-        <p class="mt-3  text-lg text-gray-500 sm:text-xl md:mt-5 text-left">
-          {introDescription}
-        </p>
+
+          <p class="mt-3  text-lg text-gray-500 sm:text-xl md:mt-5 text-left">
+            {introDescription}
+          </p>
         </LeftColumn>
-        <RightColumn>
-          <img class="" src={introImage}></img>
-        </RightColumn>
+        <RightColumn></RightColumn>
       </TwoColumn>
     </section>
   )
